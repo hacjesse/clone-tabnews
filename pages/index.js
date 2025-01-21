dc443cdeee3a54be9d06db7df48b243e0f96ca78
hacjesse <89802664+hacjesse@users.noreply.github.com>
2025-01-21 22:43:16 +0000

    adiciona um paragrafo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home () {
     return <h1>"A jornada mais longa começa com um único passo."
+        <p>so you'd better give this damn step</p>
+        <p>keep moving forward no matter what.</p>
 </h1>
 }
 
