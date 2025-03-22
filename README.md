c717da51f47544c4435b4f9d9ecf14e39026be4e
hacjesse <89802664+hacjesse@users.noreply.github.com>
2025-03-22 10:05:53 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto massa vai ser desenvolvido
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-function Home () {
-    return <h1>"A jornada mais longa começa com um único passo."
-        <p>so you'd better give this damn step</p>
-        <p>keep moving forward no matter what.</p>
-</h1>
+function Home() {
+  return (
+    <h1>
+      "A jornada mais longa começa com um único passo."
+      <p>so you'd better give this damn step</p>
+      <p>keep moving forward no matter what.</p>
+    </h1>
+  );
 }
 
 export default Home;
